9055be47679544ce1e026b0269d379a18ee78485
elmir mammadli <elmir.memmedov6071@gmail.com>
2024-10-16 13:19:21 +0000

    AppJs

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ $(document).ready(function () {
 
     $(document).on('keypress', function (e) {
         if (e.which == 13) {
-            let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=050215f83eaa3500e1575b3890c5a60e&units=metric`;
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myweather").val()}&appid=050215f83eaa3500e1575b3890c5a60e&units=metric`;
             async function myWeather() {
                 let responce = await fetch(url);
                 let data = await responce.json();
                 $("#city").html(data.name);
                 $("#temp").html(data.main.temp);
                 $("#feels").html(data.main.feels_like);
-                $("#myWeather").val("");
+                $("#myweather").val("");
                 console.log(data);
             };
             myWeather();
@@ -21,14 +21,14 @@ $(document).ready(function () {
 
     $("#btn").click(function (e) {
         e.preventDefault();
-        let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=050215f83eaa3500e1575b3890c5a60e&units=metric`;
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myweather").val()}&appid=050215f83eaa3500e1575b3890c5a60e&units=metric`;
         async function myWeather() {
             let responce = await fetch(url);
             let data = await responce.json();
             $("#city").html(data.name);
             $("#temp").html(data.main.temp);
             $("#feels").html(data.main.feels_like);
-            $("#myWeather").val("");
+            $("#myweather").val("");
             console.log(data);
         };
         myWeather();
